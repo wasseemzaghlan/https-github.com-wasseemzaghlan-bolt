cc276f717c39b1e0d0c72a48f73b383cf032bcd1
wasseemzaghlan <wasseem.it.dev@gmail.com>
2024-12-21 15:05:48 +0300

    Initial commit from your app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+    export default function Home() {
+      return (
+        <div>
+          <h1>Welcome to Next.js!</h1>
+          <p>This is a simple home page created using Next.js.</p>
+        </div>
+      );
+    }
